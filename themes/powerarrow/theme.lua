3fbf5ac60d8b311aa3235a077f8eb8a0c12d79c5
gueckmooh <gueckmooh@pm.me>
2018-11-09 00:47:14 +0100

    Déportation battery && clock

--- themes/powerarrow/theme.lua
+++ themes/powerarrow/theme.lua
@@ -212,101 +212,18 @@ __        ___     _            _
                     |___/
 --]]
 
--------------------- {{{ clock
-local clock = awful.widget.watch(
-    -- "date +'%a %d %b %R'", 60,
-    "date +'%R'", 5,
-    function(widget, stdout)
-        widget:set_markup(markup.fontfg(theme.font, theme.fg_normal, stdout))
-    end
-)
+-------------------- {{{ Clock }}} ---------------------------------------------
 
-local clock_widget = wibox.widget {
-    {
-        clock,
-        left = dpi(4),
-        right = dpi(4),
-        widget = wibox.container.margin,
-    },
-    layout = wibox.layout.align.horizontal,
-}
--------------------- }}}
+local clock = require ("widget.clock")
+local clock_widget = clock.get_widget (theme)
+
+-------------------- {{{ End Clock }}} -----------------------------------------
 
 -------------------- {{{ Battery }}} -------------------------------------------
-local battery_icon = wibox.widget.imagebox (theme.widget_battery)
-local last_battery_check = 0
 
-local battery_notification
-local function show_battery_status()
-    awful.spawn.easy_async([[bash -c 'acpi']],
-        function(stdout, _, _, _)
-            battery_notification = naughty.notify{
-                text =  stdout,
-                title = "Battery status",
-                timeout = 5, hover_timeout = 0.5,
-                width = 200,
-            }
-        end
-    )
-end
+local battery = require ("widget.battery")
+local battery_widget = battery.get_widget (theme)
 
-local function show_battery_warning()
-    naughty.notify{
-        -- icon = HOME .. "/.config/awesome/nichosi.png",
-        -- icon_size=100,
-        text = "Huston, we have a problem",
-        title = "Battery is dying",
-        timeout = 5, hover_timeout = 0.5,
-        position = "top_right",
-        bg = "#F06060",
-        fg = "#EEE9EF",
-        width = 300,
-    }
-end
-
-local battery_text = awful.widget.watch (
-    "bash -c 'echo $(acpi) $(acpi -a)'",
-    10,
-    function (widget, stdout)
-        local _, status, charge_str, sect =
-            string.match(stdout, '(.+): (%a+), (%d?%d?%d)%%,.* Adapter 0: (%a+.%a+)')
-        local battery_info = {
-            status = status,
-            charge = tonumber(charge_str),
-            charging = sect == "on-line"
-        }
-        if battery_info.charging then
-            battery_icon:set_image (theme.widget_ac)
-        else
-            battery_icon:set_image (theme.widget_battery)
-            if battery_info.charge < 7 then
-                if os.difftime(os.time(), last_battery_check) > 300
-                then
-                    show_battery_warning ()
-                    last_battery_check = os.time ()
-                end
-            end
-        end
-        widget:set_markup(markup.fontfg(theme.font, theme.fg_normal,
-                                        tostring (battery_info.charge).."%"))
-    end
-)
-
-local battery_widget = wibox.widget {
-    battery_icon,
-    {
-        battery_text,
-        left = dpi(4),
-        right = dpi(4),
-        widget = wibox.container.margin,
-    },
-    layout = wibox.layout.align.horizontal,
-}
-
-battery_widget:connect_signal("mouse::enter", function()
-                                  show_battery_status() end)
-battery_widget:connect_signal("mouse::leave", function()
-                                  naughty.destroy(battery_notification) end)
 -------------------- {{{ End Battery }}} ---------------------------------------
 
 -------------------- {{{ Pulse Audio }}} ---------------------------------------
@@ -412,5 +329,3 @@ end
 
 
 return theme
-
--- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
