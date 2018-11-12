765bc49f8fa7f100111ff860fc4ddced02f1504d
Enzo Brignon <brignone@nia.lan>
2018-11-12 16:09:53 +0100

    ajout / devant .config

--- themes/powerarrow/theme.lua
+++ themes/powerarrow/theme.lua
@@ -19,7 +19,7 @@ local arrow           = separators.arrow_left
 local theme = {}
 
 theme.name = "powerarrow"
--- theme.dir = os.getenv ("HOME") .. ".config/awesome/themes/" .. theme.name
+-- theme.dir = os.getenv ("HOME") .. "/.config/awesome/themes/" .. theme.name
 theme.dir = "/home/brignone/git/yet_another_awesome_config/themes/" .. theme.name -- CONFIG
 
 theme.font          = "sans 8"
