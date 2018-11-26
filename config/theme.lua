8dc16e3ea98b4436045a9bcc03b7f2398db0c49e
gueckmooh <gueckmooh@pm.me>
2018-11-26 23:05:52 +0100

    Modification fucking gaps

--- config/theme.lua
+++ config/theme.lua
@@ -96,11 +96,15 @@ awful.rules.rules = {
 
     -- Add titlebars to normal clients and dialogs
     { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = true }
+      }, properties = { titlebars_enabled = true, }
     },
 
     { rule_any = {name = { "QuakeDD" }
       }, properties = { titlebars_enabled = false }
+    },
+
+    { rule_any = {class = { "URxvt", "Emacs" }
+                 }, properties = { size_hints_honor = false }
     },
 
     { rule_any = {class = { "mpv" }
